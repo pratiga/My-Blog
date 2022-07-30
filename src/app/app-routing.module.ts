a29f86df74cda09ef6b144ef57bd0400ffcc9ba9
pratiga <gaitrisanu2468@gmail.com>
2022-07-30 21:37:21 +0545

    routing modules
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
+import { HomeComponent } from './admin/home/home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { PostsComponent } from './admin/dashboard/posts/posts.component';
+import { PagesComponent } from './admin/dashboard/pages/pages.component';
+import { CommentsComponent } from './admin/dashboard/comments/comments.component';
+import { SettingsComponent } from './admin/dashboard/settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",component:HomeComponent,
+    children:[
+      {path:"dashboard", component:DashboardComponent},
+      {path:"posts", component:PostsComponent},
+      {path:"pages", component:PagesComponent},
+      {path:"comments", component:CommentsComponent},
+      {path:"settings", component:SettingsComponent},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
